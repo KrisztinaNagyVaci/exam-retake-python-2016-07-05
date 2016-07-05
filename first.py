b171eb1b7a779181b9da4c77997d0536ef2010b4
Kriszti <krisztina.vaci@gmail.com>
2016-07-05 08:50:18 +0200

    first

--- first.py
+++ first.py
@@ -4,3 +4,19 @@
 # It should raise an error if the parameter is not a list
 
 list1 = [1, 2, 3, 4]
+list2 = [1, 3, 5]
+nonlist = 'I am a string'
+
+def returnTrueifElementsEven(input_list):
+    if type(input_list) != list:
+        raise TypeError('The argument is not a list')
+    list_of_evens = []
+    for element in input_list:
+        if element % 2 == 0:
+            list_of_evens.append(element)
+    if len(list_of_evens) == 0:
+        return True
+    else:
+        return False
+
+print(returnTrueifElementsEven(list1))
