6ff512d6aede3c34e4d948437fb661d324c88355
Kriszti <krisztina.vaci@gmail.com>
2016-07-05 09:05:05 +0200

    second

--- second.py
+++ second.py
@@ -2,3 +2,15 @@
 # it should count the words in the file and is should return it as a number
 # if the file not exists or any other error occures return 0
 
+def countWordsinFile(filename):
+    try:
+        f = open(filename)
+        text = f.read()
+        f.close()
+        list_of_words = []
+        list_of_words = text.split()
+        return len(list_of_words)
+    except:
+        return 0
+
+print(countWordsinFile('1.txt'))
