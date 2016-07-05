f9d1641143aaf0bf78663ad66302bb9d61ea7a0c
Kriszti <krisztina.vaci@gmail.com>
2016-07-05 09:14:25 +0200

    third

--- third.py
+++ third.py
@@ -1,5 +1,10 @@
 # Write unittests for this function:
 
+import unittest
+
+list1 = [1, 2, 3, 4]
+number1 = 3
+
 def filter_smaller_than_number(original_list, number):
   if type(original_list) != list:
     return []
@@ -9,4 +14,18 @@ def filter_smaller_than_number(original_list, number):
       output.append(element)
   return output
 
-filter_smaller_than_number()
+print(filter_smaller_than_number(list1, number1))
+
+class MyTest(unittest.TestCase):
+    def test_list_input_was_given(self):
+        self.assertEqual(filter_smaller_than_number([1, 2, 3, 4], 3), [1, 2])
+
+    def test_nonlist_input_was_given(self):
+        self.assertEqual(filter_smaller_than_number('hello', 3), [])
+
+    def test_input_number_is_smaller_than_any_list_elements(self):
+        self.assertEqual(filter_smaller_than_number([3, 4, 5, 6], 2), [])
+
+
+if __name__ == '__main__':
+    unittest.main()
